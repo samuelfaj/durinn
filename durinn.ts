37826721d5664a1d310ca9009769200cbec90dff
Samuel Fajreldines <samuelfaj@icloud.com>
2019-06-07 11:56:05 -0300

    Adding environment

--- durinn.ts
+++ durinn.ts
@@ -11,12 +11,12 @@ const Durinn: {[a:string]: any, sequelize: Sequelize} = {
   name: `Durinn Framework v.1.0`,
   description: `In this file you can store global variables as database configuration or global user object`,
   sequelize: new Sequelize(
-    config[process.env.NODE_ENV || 'production']['database'],
-    config[process.env.NODE_ENV || 'production']['username'],
-    config[process.env.NODE_ENV || 'production']['password'],
+    config[process.env.NODE_ENV || 'development']['database'],
+    config[process.env.NODE_ENV || 'development']['username'],
+    config[process.env.NODE_ENV || 'development']['password'],
     {
-      host: config[process.env.NODE_ENV || 'production']['host'],
-      dialect: config[process.env.NODE_ENV || 'production']['dialect'],
+      host: config[process.env.NODE_ENV || 'development']['host'],
+      dialect: config[process.env.NODE_ENV || 'development']['dialect'],
       pool: { max: 10 }
     }
   )
